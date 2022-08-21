80c4b27471242237929f951ae021cc307cb57b03
coldfish5630 <layan.schicksal@gmail.com>
2022-08-21 12:52:14 +0900

    test

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-function setup() {
+function setup () {
+  let button
   createCanvas(700, 700)
   background(200)
+  button = createButton('changeBG')
 }
 
-function draw() {
+function draw () {
   if (mouseIsPressed) {
-    fill(0, 0, 0)
+    fill(0)
+    stroke(0)
+    strokeWeight(18)
     line(mouseX, mouseY, pmouseX, pmouseY)
   }
 }
