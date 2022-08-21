6defd2844bd155d2297a43cbdc94b4153c15f4d6
coldfish5630 <layan.schicksal@gmail.com>
2022-08-21 14:28:01 +0900

    test

--- script.js
+++ script.js
@@ -1,34 +1,30 @@
-<<<<<<< HEAD
+let color_button
+let color_picker
+
 function setup () {
-  let button
-  createCanvas(700, 700)
-  background(200)
-  button = createButton('changeBG')
-=======
-
-function setup() {
   createCanvas(700, 700)
   background(200)
 
-  let color_button
   color_button = createButton('Change Color')
   color_button.position(10, 710)
-  color_button.mouseClicked(change_background)
->>>>>>> 0cbf19888e5f9bbd8b0f0dd25017f09996556a0a
+  color_button.mousePressed(change_background)
+
+  color_picker = createColorPicker('#FFFFFF')
+  color_picker.position(150, height + 10)
 }
 
 function draw () {
   if (mouseIsPressed) {
     fill(0)
-    stroke(0)
+    stroke(color_picker.color())
     strokeWeight(18)
     line(mouseX, mouseY, pmouseX, pmouseY)
   }
 }
 
-function change_background() {
+function change_background () {
   r = random(255)
   g = random(255)
   b = random(255)
   background(r, g, b)
-} 
+}
