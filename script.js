548140296e0ac64e2e400b86ff0124729f2b6ec9
choyz1024 <nagatoyuki1024@gmail.com>
2022-08-21 11:46:34 +0800

    btn_test

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+
 function setup() {
   createCanvas(700, 700)
   background(200)
+
+  let color_button
+  color_button = createButton('Change Color')
+  color_button.position(10, 710)
+  color_button.mouseClicked(change_background)
 }
 
 function draw() {
@@ -9,3 +15,10 @@ function draw() {
     line(mouseX, mouseY, pmouseX, pmouseY)
   }
 }
+
+function change_background() {
+  r = random(255)
+  g = random(255)
+  b = random(255)
+  background(r, g, b)
+} 
